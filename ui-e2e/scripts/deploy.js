b9ad9e5332f9308ab458b045e82fa6fc7d8dcf32
rzadp <przemek@ethworks.io>
2019-05-21 16:31:42 +0200

    Renames

--- ui-e2e/scripts/deploy.js
+++ ui-e2e/scripts/deploy.js
@@ -1,14 +1,12 @@
 const path = require('path')
 const shell = require('shelljs')
 
-const envsDir = path.join(__dirname, '../contracts')
+const envsDir = path.join(__dirname, '../envs')
 const deployContractsDir = path.join(__dirname, '../../contracts-2.1.0/deploy')
-const erc20ScriptDir = path.join(__dirname, '../contracts')
 
 shell.cp(path.join(envsDir, 'contracts.env'), path.join(deployContractsDir, '.env'))
 shell.cd(deployContractsDir)
 shell.exec('node deploy.js')
-shell.cd(erc20ScriptDir)
 shell.exec('node deployERC20.js')
 shell.cd(deployContractsDir)
 shell.rm('.env')
