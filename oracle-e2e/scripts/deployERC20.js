16423bdedc16a6e9499a0d6935cb195a80644c0d
rzadp <przemek@ethworks.io>
2019-05-21 10:33:31 +0200

    Using main contracts submodule in oracle-e2e.

--- oracle-e2e/scripts/deployERC20.js
+++ oracle-e2e/scripts/deployERC20.js
@@ -1,19 +1,19 @@
 /* eslint import/no-unresolved: 0  node/no-missing-require: 0 */
 const path = require('path')
 require('dotenv').config({
-  path: path.join(__dirname, '../submodules/poa-bridge-contracts/deploy/.env')
+  path: path.join(__dirname, '../../contracts/deploy/.env')
 })
 
 const {
   deployContract,
   sendRawTx,
   privateKeyToAddress
-} = require('../submodules/poa-bridge-contracts/deploy/src/deploymentUtils')
+} = require('../../contracts/deploy/src/deploymentUtils')
 const {
   web3Foreign,
   deploymentPrivateKey
-} = require('../submodules/poa-bridge-contracts/deploy/src/web3')
-const POA20 = require('../submodules/poa-bridge-contracts/build/contracts/ERC677BridgeToken.json')
+} = require('../../contracts/deploy/src/web3')
+const POA20 = require('../../contracts/build/contracts/ERC677BridgeToken.json')
 const { user } = require('../constants.json')
 
 const { DEPLOYMENT_ACCOUNT_PRIVATE_KEY } = process.env
