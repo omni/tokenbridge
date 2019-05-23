b89c7f7d090898d757691ec49565d099b531837f
rzadp <przemek@ethworks.io>
2019-05-23 12:05:08 +0200

    test

--- ui-e2e/run-tests.sh
+++ ui-e2e/run-tests.sh
@@ -2,10 +2,14 @@
 cd $(dirname $0)
 
 docker-compose down
-docker-compose up -d --build --force-recreate
+docker-compose up -d --build
 node ./scripts/blocks.js &
 
 docker-compose run e2e yarn workspace ui-e2e run deploy
+ps | grep node | grep -v grep | awk '{print "kill " $1}' | sh
+docker-compose down
+exit 0
+
 docker-compose run -d bridge npm run watcher:signature-request
 docker-compose run -d bridge npm run watcher:collected-signatures
 docker-compose run -d bridge npm run watcher:affirmation-request
