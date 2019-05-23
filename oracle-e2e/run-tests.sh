b085c00616a933e18fe70e8d7757c5d52b5cbaa8
rzadp <przemek@ethworks.io>
2019-05-23 15:01:17 +0200

    Test script

--- oracle-e2e/run-tests.sh
+++ oracle-e2e/run-tests.sh
@@ -1,9 +1,17 @@
 cd $(dirname $0)
 
+ps | grep node | grep -v grep | awk '{print "kill " $1}' | sh
 docker-compose down
-docker-compose up -d --build --force-recreate
+
+docker-compose up -d --build
 
 docker-compose run e2e yarn workspace oracle-e2e run deploy
+
+ps | grep node | grep -v grep | awk '{print "kill " $1}' | sh
+docker-compose down
+exit 0
+
+
 docker-compose run -d bridge npm run watcher:signature-request
 docker-compose run -d bridge npm run watcher:collected-signatures
 docker-compose run -d bridge npm run watcher:affirmation-request
