2159fc1a570c4af28931d3ff7ad0d82257649342
rzadp <przemek@ethworks.io>
2019-06-03 09:57:27 +0200

    Fixed down.

--- oracle-e2e/run-tests.sh
+++ oracle-e2e/run-tests.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 docker-compose -f ../e2e-commons/docker-compose.yml run e2e yarn workspace oracle-e2e run deploy
 
 docker-compose -f ../e2e-commons/docker-compose.yml run e2e yarn workspace oracle-e2e run start
-
 rc=$?
-docker-compose down
+
+../e2e-commons/down.sh
 exit $rc
