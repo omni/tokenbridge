7e937bdefa7b26348a9c68a565eb8df068fbb504
rzadp <przemek@ethworks.io>
2019-06-04 10:43:27 +0200

    Clean up deploy script

--- e2e-commons/scripts/deploy.sh
+++ e2e-commons/scripts/deploy.sh
@@ -6,50 +6,26 @@ CONTRACTS_PATH="../../contracts"
 DEPLOY_PATH="$CONTRACTS_PATH/deploy"
 ENVS_PATH="../envs"
 
-# shell.cp(path.join(envsDir, 'contracts.env'), path.join(deployContractsDir, '.env'))
+echo -e "\n\n############ Deploying native-to-erc ############\n"
 cp "$ENVS_PATH/native-to-erc.env" "$DEPLOY_PATH/.env"
-
-# shell.cd(deployContractsDir)
 cd "$DEPLOY_PATH"
-
-# shell.exec('node deploy.js')
-echo -e "\n############ Deploying native-to-erc ############"
 node deploy.js
-
-# shell.cd(__dirname)
 cd - > /dev/null
 
-# shell.exec('node deployERC20.js')
-echo -e "\n############ Deploying erc20 ############"
+echo -e "\n\n############ Deploying erc20 and erc-to-erc ############\n"
 node deployERC20.js
-
-# shell.cd(deployContractsDir)
-cd "$DEPLOY_PATH"
-
-# shell.rm('.env')
-rm .env
-
-# shell.cp(path.join(envsDir, 'erc-contracts.env'), path.join(deployContractsDir, '.env'))
-cd - > /dev/null
 cp "$ENVS_PATH/erc-to-erc.env" "$DEPLOY_PATH/.env"
 cd "$DEPLOY_PATH"
-
-# shell.exec('node deploy.js')
-echo -e "\n############ Deploying erc-to-erc ############"
 node deploy.js
-
-# shell.rm('.env')
-rm .env
-
 cd - > /dev/null
-# shell.cp(
-#   path.join(envsDir, 'erc-native-contracts.env'),
-#   path.join(deployContractsDir, '.env')
-# )
+
+echo -e "\n\n############ Deploying block reward ############\n"
+cd "$DEPLOY_PATH"
+node src/utils/deployBlockReward.js
+cd - > /dev/null
+
+echo -e "\n\n############ Deploying erc-to-native ############\n"
 cp "$ENVS_PATH/erc-to-native.env" "$DEPLOY_PATH/.env"
 cd "$DEPLOY_PATH"
-
-echo -e "\n############ Deploying block reward ############"
-node src/utils/deployBlockReward.js
-echo -e "\n############ Deploying erc-to-native ############"
 node deploy.js
+cd - > /dev/null
