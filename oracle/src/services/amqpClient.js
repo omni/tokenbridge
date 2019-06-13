ae6692abcd20b45a4bdb0cb67928feb316e27746
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-06-13 14:29:40 -0300

    Add unit test for retry queue

--- oracle/src/services/amqpClient.js
+++ oracle/src/services/amqpClient.js
@@ -44,18 +44,13 @@ function connectSenderToQueue({ queueName, cb }) {
           ackMsg: job => channelWrapper.ack(job),
           nackMsg: job => channelWrapper.nack(job, false, true),
           scheduleForRetry: async (data, msgRetries = 0) => {
-            const retries = msgRetries + 1
-            const delay = getRetrySequence(retries) * 1000
-            const retryQueue = `${queueName}-retry-${delay}`
-            await channel.assertQueue(retryQueue, {
-              durable: true,
-              deadLetterExchange,
-              messageTtl: delay,
-              expires: delay * 10
-            })
-            await channelWrapper.sendToQueue(retryQueue, data, {
-              persistent: true,
-              headers: { 'x-retries': retries }
+            await generateRetry({
+              data,
+              msgRetries,
+              channelWrapper,
+              channel,
+              queueName,
+              deadLetterExchange
             })
           }
         })
@@ -64,8 +59,32 @@ function connectSenderToQueue({ queueName, cb }) {
   })
 }
 
+async function generateRetry({
+  data,
+  msgRetries,
+  channelWrapper,
+  channel,
+  queueName,
+  deadLetterExchange
+}) {
+  const retries = msgRetries + 1
+  const delay = getRetrySequence(retries) * 1000
+  const retryQueue = `${queueName}-retry-${delay}`
+  await channel.assertQueue(retryQueue, {
+    durable: true,
+    deadLetterExchange,
+    messageTtl: delay,
+    expires: delay * 10
+  })
+  await channelWrapper.sendToQueue(retryQueue, data, {
+    persistent: true,
+    headers: { 'x-retries': retries }
+  })
+}
+
 module.exports = {
   connectWatcherToQueue,
   connectSenderToQueue,
-  connection
+  connection,
+  generateRetry
 }
