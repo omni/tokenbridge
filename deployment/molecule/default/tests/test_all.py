d73c5877461e9b1a5f7681b822d490b3215f8ebd
rzadp <przemek@ethworks.io>
2019-07-09 11:42:28 +0200

    Lint.

--- deployment/molecule/default/tests/test_all.py
+++ deployment/molecule/default/tests/test_all.py
@@ -19,6 +19,7 @@ def test_user(host):
     assert host.user('poadocker').exists
     assert 'docker' in host.user('poadocker').groups
 
+
 def test_logging(host):
     assert host.file('/etc/rsyslog.d/30-docker.conf').exists
     assert host.file('/etc/rsyslog.d/35-docker-remote-logging.conf').exists
