fc92c0af9dc7cb4e59a7f1779afc8da8d9c33ecf
rzadp <przemek@ethworks.io>
2019-07-24 11:24:42 +0200

    Stopped silently swallowing errors

--- monitor/utils/ercUtils.js
+++ monitor/utils/ercUtils.js
@@ -1,14 +1,10 @@
 const { ERC_TYPES } = require('../../commons')
 
 const getTokenType = async (contract, bridgeAddress) => {
-  try {
-    const bridgeContract = await contract.methods.bridgeContract().call()
-    if (bridgeContract === bridgeAddress) {
-      return ERC_TYPES.ERC677
-    } else {
-      return ERC_TYPES.ERC20
-    }
-  } catch (e) {
+  const bridgeContract = await contract.methods.bridgeContract().call()
+  if (bridgeContract === bridgeAddress) {
+    return ERC_TYPES.ERC677
+  } else {
     return ERC_TYPES.ERC20
   }
 }
