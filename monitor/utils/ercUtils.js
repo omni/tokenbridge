4b07f2efca7f7723663187fbc47c26a4908001c7
rzadp <przemek@ethworks.io>
2019-07-24 11:47:53 +0200

    Revert "Stopped silently swallowing errors"
    fc92c0af9dc7cb4e59a7f1779afc8da8d9c33ecf

--- monitor/utils/ercUtils.js
+++ monitor/utils/ercUtils.js
@@ -1,10 +1,14 @@
 const { ERC_TYPES } = require('../../commons')
 
 const getTokenType = async (contract, bridgeAddress) => {
-  const bridgeContract = await contract.methods.bridgeContract().call()
-  if (bridgeContract === bridgeAddress) {
-    return ERC_TYPES.ERC677
-  } else {
+  try {
+    const bridgeContract = await contract.methods.bridgeContract().call()
+    if (bridgeContract === bridgeAddress) {
+      return ERC_TYPES.ERC677
+    } else {
+      return ERC_TYPES.ERC20
+    }
+  } catch (e) {
     return ERC_TYPES.ERC20
   }
 }
