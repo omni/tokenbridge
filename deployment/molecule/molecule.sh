c9d100491cbfeee06512029350a874a0b32a4146
Przemyslaw Rzad <przemek@ethworks.io>
2019-07-26 10:14:25 +0200

    Ultimate E2E tests (#158)
    * Ultimate e2e for native to erc type of bridge
    
    * One job
    
    * Try to run the tests
    
    * Run on CI
    
    * Docs
    
    * Cosmetics
    
    * Final changes
    
    * Revert the changes
    
    * Networks
    
    * Waiting for Oracle
    
    * One job
    
    * Initialize the contracts submodule
    
    * Run on ci
    
    * comment
    
    * Sleep.
    
    * ultimate network
    
    * Docker localhost no longer needed
    
    * Final changes
    
    * Timeout
    
    * Naming
    
    * Sleep task.

--- deployment/molecule/molecule.sh
+++ deployment/molecule/molecule.sh
@@ -3,10 +3,9 @@ cd $(dirname $0)
 set -e # exit when any command fails
 
 CODEBASE_BRANCH=${CIRCLE_BRANCH-$(git symbolic-ref --short HEAD)}
-DOCKER_LOCALHOST=${DOCKER_LOCALHOST-localhost}
 
 while [ "$1" != "" ]; do
-  docker-compose build && docker-compose run -e CODEBASE_BRANCH=$CODEBASE_BRANCH -e DOCKER_LOCALHOST=$DOCKER_LOCALHOST molecule_runner /bin/bash -c "molecule test --scenario-name $1"
+  docker-compose build && docker-compose run -e CODEBASE_BRANCH=$CODEBASE_BRANCH molecule_runner /bin/bash -c "molecule test --scenario-name $1"
 
   shift # Shift all the parameters down by one
 done
