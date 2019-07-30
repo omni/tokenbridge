9b8eec9a0ec6516bc9ac69f992b90d73d74d10d0
rzadp <przemek@ethworks.io>
2019-07-30 15:55:22 +0200

    grep errors

--- monitor-e2e/erc-to-erc.sh
+++ monitor-e2e/erc-to-erc.sh
@@ -5,20 +5,20 @@ echo "Test case - Web Interface should return balances"
 OUTPUT=$(curl -s http://$URL/)
 grep -q home <<< "$OUTPUT"
 grep -q foreign <<< "$OUTPUT"
-# (! grep -q error <<< "$OUTPUT")
+(! grep -q error <<< "$OUTPUT")
 
 echo "Test case - Web Interface should return validators"
 OUTPUT=$(curl -s http://$URL/validators)
 grep -q home <<< "$OUTPUT"
 grep -q foreign <<< "$OUTPUT"
-# (! grep -q error <<< "$OUTPUT")
+(! grep -q error <<< "$OUTPUT")
 
 echo "Test case - Web Interface should return eventsStats"
 OUTPUT=$(curl -s http://$URL/eventsStats)
 grep -q lastChecked <<< "$OUTPUT"
-# (! grep -q error <<< "$OUTPUT")
+(! grep -q error <<< "$OUTPUT")
 
 echo "Test case - Web Interface should return alerts"
 OUTPUT=$(curl -s http://$URL/alerts)
 grep -q lastChecked <<< "$OUTPUT"
-# (! grep -q error <<< "$OUTPUT")
+(! grep -q error <<< "$OUTPUT")
