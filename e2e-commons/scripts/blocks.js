13dd14abf878f0d324a24a315cec9f85a8a81d78
rzadp <przemek@ethworks.io>
2019-08-02 11:07:43 +0200

    Trying to use block generator address

--- e2e-commons/scripts/blocks.js
+++ e2e-commons/scripts/blocks.js
@@ -3,14 +3,32 @@ const { generateNewBlock } = require('../utils')
 
 const homeWeb3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8541'))
 const foreignWeb3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8542'))
-const {user} = require('../constants.json');
+const {user, blockGenerator} = require('../constants.json');
+
 homeWeb3.eth.accounts.wallet.add(user.privateKey)
 foreignWeb3.eth.accounts.wallet.add(user.privateKey)
+homeWeb3.eth.accounts.wallet.add(blockGenerator.privateKey)
+foreignWeb3.eth.accounts.wallet.add(blockGenerator.privateKey)
 
 function main() {
+  await homeWeb3.eth.sendTransaction({
+    from: user.address,
+    to: blockGenerator.address,
+    gasPrice: '1',
+    gas: '21000',
+    value: '2100000000'
+  })
+  await foreignWeb3.eth.sendTransaction({
+    from: user.address,
+    to: blockGenerator.address,
+    gasPrice: '1',
+    gas: '21000',
+    value: '2100000000'
+  })
+
   setTimeout(async () => {
-    generateNewBlock(homeWeb3, user.address)
-    generateNewBlock(foreignWeb3, user.address)
+    generateNewBlock(homeWeb3, blockGenerator.address)
+    generateNewBlock(foreignWeb3, blockGenerator.address)
     main()
   }, 5000)
 }
