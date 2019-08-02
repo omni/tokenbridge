8dc2797c0115513709bdc6569337de5cfa8935d9
rzadp <przemek@ethworks.io>
2019-08-02 11:20:00 +0200

    Fixed

--- e2e-commons/scripts/blocks.js
+++ e2e-commons/scripts/blocks.js
@@ -10,28 +10,28 @@ foreignWeb3.eth.accounts.wallet.add(user.privateKey)
 homeWeb3.eth.accounts.wallet.add(blockGenerator.privateKey)
 foreignWeb3.eth.accounts.wallet.add(blockGenerator.privateKey)
 
-function main() {
-  await homeWeb3.eth.sendTransaction({
-    from: user.address,
-    to: blockGenerator.address,
-    gasPrice: '1',
-    gas: '21000',
-    value: '2100000000'
-  })
-  await foreignWeb3.eth.sendTransaction({
-    from: user.address,
-    to: blockGenerator.address,
-    gasPrice: '1',
-    gas: '21000',
-    value: '2100000000'
-  })
-
+async function main() {
   setTimeout(async () => {
     generateNewBlock(homeWeb3, blockGenerator.address)
     generateNewBlock(foreignWeb3, blockGenerator.address)
     main()
   }, 5000)
 }
+
+homeWeb3.eth.sendTransaction({
+  from: user.address,
+  to: blockGenerator.address,
+  gasPrice: '1',
+  gas: '21000',
+  value: '2100000000'
+})
+foreignWeb3.eth.sendTransaction({
+  from: user.address,
+  to: blockGenerator.address,
+  gasPrice: '1',
+  gas: '21000',
+  value: '2100000000'
+})
 
 main()
 
