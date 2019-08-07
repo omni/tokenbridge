f08412cd51128bb3646db777138fd087362c7fa8
rzadp <przemek@ethworks.io>
2019-08-07 15:19:50 +0200

    axios

--- monitor-e2e/test/nativeToErc.js
+++ monitor-e2e/test/nativeToErc.js
@@ -1,6 +1,25 @@
 const assert = require('assert')
+const axios = require('axios')
 
 describe('NATIVE TO ERC', () => {
+  describe('balances', async () => {
+    let output
+
+    beforeEach(async () => {
+      output = await axios.get('http://localhost:3010')
+    })
+
+    it('should be ok', () => {
+      assert(output === 'lala')
+    })
+  })
+
+  describe('validators', async () => {})
+
+  describe('eventsStats', async () => {})
+
+  describe('alerts', async () => {})
+
   it('should work', async () => {
     assert(true)
   })
