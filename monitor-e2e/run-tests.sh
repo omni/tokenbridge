c7f050a088017d665b5094154a0491a6288c46f5
rzadp <przemek@ethworks.io>
2019-08-07 16:56:34 +0200

    First deploy

--- monitor-e2e/run-tests.sh
+++ monitor-e2e/run-tests.sh
@@ -1,6 +1,6 @@
 cd $(dirname $0)
 
-../e2e-commons/up.sh monitor deploy
+../e2e-commons/up.sh deploy monitor
 
 docker-compose -f ../e2e-commons/docker-compose.yml run e2e yarn workspace monitor-e2e run start
 rc=$?
