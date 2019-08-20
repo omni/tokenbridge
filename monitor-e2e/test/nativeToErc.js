742d7f0dfc21c2ab18b16296a546dae6e84dd9c3
rzadp <przemek@ethworks.io>
2019-08-20 13:50:08 +0200

    Little refactor

--- monitor-e2e/test/nativeToErc.js
+++ monitor-e2e/test/nativeToErc.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 const Web3 = require('web3')
 const shell = require('shelljs')
 const { nativeToErcBridge, user, homeRPC, foreignRPC } = require('../../e2e-commons/constants.json')
-const { generateNewBlock } = require('../../e2e-commons/utils')
 
 const baseUrl = nativeToErcBridge.monitor
 const HOME_BRIDGE_ADDRESS = nativeToErcBridge.home
@@ -11,6 +10,13 @@ const homeWeb3 = new Web3(new Web3.providers.HttpProvider(homeRPC.URL))
 const foreignWeb3 = new Web3(new Web3.providers.HttpProvider(foreignRPC.URL))
 homeWeb3.eth.accounts.wallet.add(user.privateKey)
 foreignWeb3.eth.accounts.wallet.add(user.privateKey)
+
+const checkAll = () => {
+  // the different container names comes from macOS/linux docker differences and/or different docker-compose versions
+  ;['e2e-commons_monitor_1', 'e2e_commons_monitor_1', 'e2ecommons_monitor_1'].forEach(container =>
+    shell.exec(`docker exec ${container} yarn check-all`)
+  )
+}
 
 describe('NATIVE TO ERC', () => {
   let data
@@ -23,7 +29,7 @@ describe('NATIVE TO ERC', () => {
   it('should contain totalSupply', () => assert(data.foreign.totalSupply === '0'))
 })
 
-describe.only('NATIVE TO ERC with changing state of contracts', () => {
+describe('NATIVE TO ERC with changing state of contracts', () => {
   let data
 
   before(async () => {
@@ -31,8 +37,8 @@ describe.only('NATIVE TO ERC with changing state of contracts', () => {
   })
 
   it('should change balanceDiff', async () => {
-    // send transaction to home chain
-    const depositTx = await homeWeb3.eth.sendTransaction({
+    assert(data.balanceDiff === 0)
+    await homeWeb3.eth.sendTransaction({
       from: user.address,
       to: HOME_BRIDGE_ADDRESS,
       gasPrice: '1',
@@ -40,20 +46,8 @@ describe.only('NATIVE TO ERC with changing state of contracts', () => {
       value: '1000000000000000000'
     })
 
-    // Send a trivial transaction to generate a new block since the watcher
-    // is configured to wait 1 confirmation block
-    // await generateNewBlock(homeWeb3, user.address)
-
-    console.log('first should be zero...')
-    assert(data.balanceDiff === 0)
-
-    console.log('starting check all')
-    shell.exec('docker exec e2e-commons_monitor_1 yarn check-all || docker exec e2e_commons_monitor_1 yarn check-all || docker exec e2ecommons_monitor_1 yarn check-all') // The difference between macOS and linux docker
-    console.log('check all completed')
-
+    checkAll()
     ;({ data } = await axios.get(`${baseUrl}`))
-
-    console.log('should not be zero...')
     assert(data.balanceDiff !== 0)
   })
 })
