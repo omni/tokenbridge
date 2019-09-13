a407e89e304bd4715f544ac497955aff275e3126
rzadp <przemek@ethworks.io>
2019-09-13 09:30:35 +0200

    Remove ERC20_TOKEN_ADDRESS from env variables

--- oracle/scripts/initialChecks.js
+++ oracle/scripts/initialChecks.js
@@ -1,15 +1,33 @@
 require('../env')
 const Web3 = require('web3')
-const { ERC677_BRIDGE_TOKEN_ABI, getTokenType } = require('../../commons')
+const {
+  ERC677_BRIDGE_TOKEN_ABI,
+  FOREIGN_ERC_TO_ERC_ABI,
+  FOREIGN_ERC_TO_NATIVE_ABI,
+  FOREIGN_NATIVE_TO_ERC_ABI,
+  getTokenType
+} = require('../../commons')
 
 async function initialChecks() {
-  const { ERC20_TOKEN_ADDRESS, ORACLE_BRIDGE_MODE, COMMON_FOREIGN_RPC_URL, COMMON_FOREIGN_BRIDGE_ADDRESS } = process.env
+  const { ORACLE_BRIDGE_MODE, COMMON_FOREIGN_RPC_URL, COMMON_FOREIGN_BRIDGE_ADDRESS } = process.env
   const result = {}
+  const foreignWeb3 = new Web3(new Web3.providers.HttpProvider(COMMON_FOREIGN_RPC_URL))
 
   if (ORACLE_BRIDGE_MODE === 'ERC_TO_ERC') {
-    const foreignWeb3 = new Web3(new Web3.providers.HttpProvider(COMMON_FOREIGN_RPC_URL))
-    const bridgeTokenContract = new foreignWeb3.eth.Contract(ERC677_BRIDGE_TOKEN_ABI, ERC20_TOKEN_ADDRESS)
+    const bridge = new foreignWeb3.eth.Contract(FOREIGN_ERC_TO_ERC_ABI, COMMON_FOREIGN_BRIDGE_ADDRESS)
+    result.ERC20_TOKEN_ADDRESS = await bridge.methods.erc20token().call()
+  } else if (ORACLE_BRIDGE_MODE === 'ERC_TO_NATIVE') {
+    const bridge = new foreignWeb3.eth.Contract(FOREIGN_ERC_TO_NATIVE_ABI, COMMON_FOREIGN_BRIDGE_ADDRESS)
+    result.ERC20_TOKEN_ADDRESS = await bridge.methods.erc20token().call()
+  } else if (ORACLE_BRIDGE_MODE === 'NATIVE_TO_ERC') {
+    const bridge = new foreignWeb3.eth.Contract(FOREIGN_NATIVE_TO_ERC_ABI, COMMON_FOREIGN_BRIDGE_ADDRESS)
+    result.ERC20_TOKEN_ADDRESS = await bridge.methods.erc677token().call()
+  } else {
+    throw new Error(`Unsupported type of bridge: ${ORACLE_BRIDGE_MODE}`)
+  }
 
+  if (ORACLE_BRIDGE_MODE === 'ERC_TO_ERC') {
+    const bridgeTokenContract = new foreignWeb3.eth.Contract(ERC677_BRIDGE_TOKEN_ABI, result.ERC20_TOKEN_ADDRESS)
     result.foreignERC = await getTokenType(bridgeTokenContract, COMMON_FOREIGN_BRIDGE_ADDRESS)
   }
   console.log(JSON.stringify(result))
