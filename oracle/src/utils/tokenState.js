482161d124637068240d59d9475ad4b2f8f46b31
Alexander Kolotov <alexandr.kolotov@gmail.com>
2019-12-23 15:47:35 +0300

    fix linting error

--- oracle/src/utils/tokenState.js
+++ oracle/src/utils/tokenState.js
@@ -3,7 +3,7 @@ async function getTokensState(bridgeContract) {
   try {
     logger.debug('Getting bridgeable token address')
     context.bridgeableTokenAddress = await bridgeContract.methods.erc20token().call()
-    logger.debug({ context.bridgeableTokenAddress }, 'Token address obtained')
+    logger.debug({ address: context.bridgeableTokenAddress }, 'Token address obtained')
   } catch (e) {
     throw new Error(`Bridgeable token address cannot be obtained`)
   }
@@ -11,7 +11,7 @@ async function getTokensState(bridgeContract) {
   try {
     logger.debug('Getting Half Duplex token address')
     const halfDuplexErc20tokenAddress = await bridgeContract.methods.halfDuplexErc20token().call()
-    logger.debug({ halfDuplexErc20tokenAddress }, 'Half Duplex token address obtained')
+    logger.debug({ address: halfDuplexErc20tokenAddress }, 'Half Duplex token address obtained')
     if (halfDuplexErc20tokenAddress !== context.bridgeableTokenAddress) {
       context.halfDuplexTokenAddress = halfDuplexErc20tokenAddress
     } else {
