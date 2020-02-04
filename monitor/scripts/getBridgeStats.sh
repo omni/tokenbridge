bce1e6509e37d30cff5ee02eea280e35ea515205
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-02-04 02:16:27 -0300

    Use monitor docker hub image for monitor deployment (#284)

--- monitor/scripts/getBridgeStats.sh
+++ monitor/scripts/getBridgeStats.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/..
 if /usr/local/bin/docker-compose ps | grep -q -i 'monitor'; then
   for file in configs/*.env
   do
-    docker run --rm --env-file $file -v $(pwd)/responses:/mono/monitor/responses monitor_monitor /bin/bash -c 'yarn check-all'
+    docker run --rm --env-file $file -v $(pwd)/responses:/mono/monitor/responses poanetwork/tokenbridge-monitor:latest /bin/bash -c 'yarn check-all'
   done
 else
   echo "Monitor is not running, skipping checks."
