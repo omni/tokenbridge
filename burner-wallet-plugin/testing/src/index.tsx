83023ff1f48fecdbb12ebb5fd5bfea8828eb7a45
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-04-09 13:23:33 -0300

    Add env vars for BW testing

--- burner-wallet-plugin/testing/src/index.tsx
+++ burner-wallet-plugin/testing/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Asset } from '@burner-wallet/assets'
 import BurnerCore from '@burner-wallet/core'
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers'
 import { InfuraGateway, InjectedGateway } from '@burner-wallet/core/gateways'
@@ -8,53 +9,55 @@ import Exchange from '@burner-wallet/exchange'
 import { Bridge, sPOA, ERC677Asset, TokenBridgeGateway } from 'tokenbridge-plugin'
 import MetamaskPlugin from '@burner-wallet/metamask-plugin'
 
-// amb Native-to-erc677
-const kspoa = new ERC677Asset({
-  id: 'kspoa',
-  name: 'KsPoa',
-  network: '42',
-  address: '0xff94183659f549D6273349696d73686Ee1d2AC83'
+let assetIdAtHome = 'assetAtHome'
+const assetIdAtForeign = 'assetAtForeign'
+let assetAtHome: Asset
+let assetAtForeign: Asset
+
+if (process.env.REACT_APP_MODE === 'AMB_NATIVE_TO_ERC677') {
+  assetAtHome = sPOA
+  assetIdAtHome = sPOA.id
+
+  assetAtForeign = new ERC677Asset({
+    id: 'assetAtForeign',
+    name: process.env.REACT_APP_FOREIGN_TOKEN_NAME,
+    network: process.env.REACT_APP_FOREIGN_NETWORK,
+    address: process.env.REACT_APP_FOREIGN_TOKEN_ADDRESS
+  })
+} else {
+  // process.env.REACT_APP_MODE === 'AMB_ERC677_TO_ERC677'
+  assetAtHome = new ERC677Asset({
+    id: 'assetAtHome',
+    name: process.env.REACT_APP_HOME_TOKEN_NAME,
+    network: process.env.REACT_APP_HOME_NETWORK,
+    address: process.env.REACT_APP_HOME_TOKEN_ADDRESS
+  })
+
+  assetAtForeign = new ERC677Asset({
+    id: 'assetAtForeign',
+    name: process.env.REACT_APP_FOREIGN_TOKEN_NAME,
+    network: process.env.REACT_APP_FOREIGN_NETWORK,
+    address: process.env.REACT_APP_FOREIGN_TOKEN_ADDRESS
+  })
+}
+
+const testBridge = new Bridge({
+  assetA: assetIdAtHome,
+  // @ts-ignore
+  assetABridge: process.env.REACT_APP_HOME_MEDIATOR_ADDRESS,
+  assetB: assetIdAtForeign,
+  // @ts-ignore
+  assetBBridge: process.env.REACT_APP_FOREIGN_MEDIATOR_ADDRESS
 })
 
-const ambNativeToErc677 = new Bridge({
-  assetA: 'spoa',
-  assetABridge: '0x867949C3F2f66D827Ed40847FaA7B3a369370e13',
-  assetB: 'kspoa',
-  assetBBridge: '0x99FB1a25caeB9c3a5Bf132686E2fe5e27BC0e2dd'
-})
-
-// amb Stake Erc677ToErc677
-const sStake = new ERC677Asset({
-  id: 'sstake',
-  name: 'sStake',
-  network: '77',
-  address: '0xC462082Dd85b27a03f380fa4C7abE90E43A986D1'
-})
-
-const kStake = new ERC677Asset({
-  id: 'kstake',
-  name: 'kStake',
-  network: '42',
-  address: '0xEda3320c159eeF5847DE491976775EC5Fca31000'
-})
-
-const ambStakeErc677ToErc677 = new Bridge({
-  assetA: 'sstake',
-  assetABridge: '0x13CCE500C577ae275E87111c465A5caF02766dd1',
-  assetB: 'kstake',
-  assetBBridge: '0x6e424386384E84d1bAc49397B383b353e412ddAf'
-})
-
-// Core burner component
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner({ privateKey: process.env.REACT_APP_PK, saveKey: false })],
   gateways: [new InjectedGateway(), new TokenBridgeGateway(), new InfuraGateway(process.env.REACT_APP_INFURA_KEY)],
-  assets: [sPOA, kspoa, sStake, kStake]
+  assets: [assetAtHome, assetAtForeign]
 })
 
-// Exchange component
 const exchange = new Exchange({
-  pairs: [ambNativeToErc677, ambStakeErc677ToErc677]
+  pairs: [testBridge]
 })
 
 const BurnerWallet = () => <ModernUI title="Testing Wallet" core={core} plugins={[exchange, new MetamaskPlugin()]} />
