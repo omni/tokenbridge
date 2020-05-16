56e40530b52e16f3588bffd88597117aa12d0ac6
Alexander Kolotov <alexandr.kolotov@gmail.com>
2020-05-17 00:51:40 +0300

    start script reworked to improve the monitor behavior

--- monitor/scripts/getBridgeStats.sh
+++ monitor/scripts/getBridgeStats.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
+
+CONFIGDIR="configs"
+RESPONSESDIR="responses"
+IMAGETAG="latest"
+
 cd $(dirname $0)/..
 
-if /usr/local/bin/docker-compose ps | grep -q -i 'monitor'; then
-  for file in configs/*.env
+# if /usr/local/bin/docker-compose ps | grep -q -i 'monitor'; then
+  tstart=`date +"%s"`
+  
+  for file in ${CONFIGDIR}/*.env
   do
-    docker run --rm --env-file $file -v $(pwd)/responses:/mono/monitor/responses poanetwork/tokenbridge-monitor:latest /bin/bash -c 'yarn check-all'
+    echo "${file} handling..."
+    
+    bridgename=`source ${file} && echo ${MONITOR_BRIDGE_NAME}`
+    reportdir=${RESPONSESDIR}"/"${bridgename}
+    if [ ! -d ${reportdir} ]; then
+      mkdir -p ${reportdir}
+    fi
+    for json in alerts.json eventsStats.json getBalances.json validators.json; do
+      echo '{"health": false, "lastChecked": '`date +"%s"`'}' > ${reportdir}/${json}
+    done
+    # this file exists only for some bridges so it will handle it separately
+    if [ -f ${reportdir}/stuckTransfers.json ]; then
+      echo '{"health": false, "lastChecked": '`date +"%s"`'}' > ${reportdir}/stuckTransfers.json
+    fi
+
+    containername=${bridgename}"-checker"
+    docker container stats --no-stream ${containername} 2>/dev/null 1>&2
+    if [ ! "$?" == "0" ]; then
+      docker run --rm --env-file $file -v $(pwd)/${RESPONSESDIR}:/mono/monitor/responses \
+        --name ${containername} poanetwork/tokenbridge-monitor:${IMAGETAG} \
+        /bin/bash -c 'yarn check-all'
+    else
+      echo "${containername} have not finished yet" >&2
+    fi
+    
+    echo "========================================"
   done
-else
-  echo "Monitor is not running, skipping checks."
-fi
+  
+  tend=`date +"%s"`
+  tdiff=`expr ${tend} - ${tstart}`
+  echo "Total time to run: ${tdiff}"
+
+# else
+#   echo "Monitor is not running, skipping checks."
+# fi
