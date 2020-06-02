1181d779bc17021a4cf614ff2bd022ca35e70750
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-06-02 11:40:32 -0300

    Add alm initial setup

---
+++ alm/config-overrides.js
@@ -0,0 +1,9 @@
+const { override } = require('customize-cra')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+
+const disableModuleScopePlugin = () => config => {
+  config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin))
+  return config
+}
+
+module.exports = override(disableModuleScopePlugin())
