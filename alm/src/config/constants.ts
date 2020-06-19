11e926ca3519ffeca0f0ecce807475cde4d51da1
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-06-19 15:05:57 -0300

    Update foreign rpc polling interval

--- alm/src/config/constants.ts
+++ alm/src/config/constants.ts
@@ -14,7 +14,7 @@ export const HOME_EXPLORER_API: string = process.env.REACT_APP_ALM_HOME_EXPLORER
 export const FOREIGN_EXPLORER_API: string = process.env.REACT_APP_ALM_FOREIGN_EXPLORER_API || ''
 
 export const HOME_RPC_POLLING_INTERVAL: number = 5000
-export const FOREIGN_RPC_POLLING_INTERVAL: number = 15000
+export const FOREIGN_RPC_POLLING_INTERVAL: number = 5000
 export const BLOCK_RANGE: number = 50
 export const ONE_DAY_TIMESTAMP: number = 86400
 export const THREE_DAYS_TIMESTAMP: number = 259200
