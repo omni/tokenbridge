d36f4d7f3535d5a981d6abf1e0a38ed5a657c6ba
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-07-03 15:11:08 -0300

    Add getValidatorList unit tests

--- alm/src/utils/__tests__/contracts.test.ts
+++ alm/src/utils/__tests__/contracts.test.ts
@@ -1,5 +1,5 @@
 import 'jest'
-import { getRequiredBlockConfirmations, getRequiredSignatures } from '../contract'
+import { getRequiredBlockConfirmations, getRequiredSignatures, getValidatorList } from '../contract'
 import { Contract } from 'web3-eth-contract'
 import { SnapshotProvider } from '../../services/SnapshotProvider'
 
@@ -252,5 +252,218 @@ describe('getRequiredSignatures', () => {
 
     expect(result).toEqual(2)
     expect(contract.getPastEvents).toBeCalledTimes(0)
+  })
+})
+describe('getValidatorList', () => {
+  const validator1 = '0x45b96809336A8b714BFbdAB3E4B5e0fe5d839908'
+  const validator2 = '0xAe8bFfc8BBc6AAa9E21ED1E4e4957fe798BEA25f'
+  const validator3 = '0x285A6eB779be4db94dA65e2F3518B1c5F0f71244'
+  const methodsBuilder = (value: string[]) => ({
+    validatorList: () => {
+      return {
+        call: () => {
+          return value
+        }
+      }
+    }
+  })
+  test('Should return the current validator list if no events found', async () => {
+    const currentValidators = [validator1, validator2, validator3]
+    const contract = ({
+      getPastEvents: jest.fn().mockImplementation(() => []),
+      methods: methodsBuilder(currentValidators)
+    } as unknown) as Contract
+
+    const snapshotProvider = ({
+      validatorAddedEvents: () => {
+        return []
+      },
+      validatorRemovedEvents: () => {
+        return []
+      },
+      snapshotBlockNumber: () => {
+        return 10
+      }
+    } as unknown) as SnapshotProvider
+
+    const list = await getValidatorList(contract, 20, snapshotProvider)
+
+    expect(list.length).toEqual(3)
+    expect(list).toEqual(expect.arrayContaining(currentValidators))
+    expect(contract.getPastEvents).toBeCalledTimes(2)
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorAdded', {
+      fromBlock: 20
+    })
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorRemoved', {
+      fromBlock: 20
+    })
+  })
+  test('If validator was added later from snapshot it should not include it', async () => {
+    const currentValidators = [validator1, validator2, validator3]
+    const contract = ({
+      getPastEvents: jest.fn().mockImplementation(() => []),
+      methods: methodsBuilder(currentValidators)
+    } as unknown) as Contract
+
+    const snapshotProvider = ({
+      validatorAddedEvents: () => {
+        return [
+          {
+            blockNumber: 9,
+            returnValues: {
+              validator: validator3
+            },
+            event: 'ValidatorAdded'
+          }
+        ]
+      },
+      validatorRemovedEvents: () => {
+        return []
+      },
+      snapshotBlockNumber: () => {
+        return 10
+      }
+    } as unknown) as SnapshotProvider
+
+    const list = await getValidatorList(contract, 5, snapshotProvider)
+
+    expect(list.length).toEqual(2)
+    expect(list).toEqual(expect.arrayContaining([validator1, validator2]))
+    expect(contract.getPastEvents).toBeCalledTimes(2)
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorAdded', {
+      fromBlock: 11
+    })
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorRemoved', {
+      fromBlock: 11
+    })
+  })
+  test('If validator was added later from chain it should not include it', async () => {
+    const currentValidators = [validator1, validator2, validator3]
+    const contract = ({
+      getPastEvents: jest.fn().mockImplementation(event => {
+        if (event === 'ValidatorAdded') {
+          return [
+            {
+              blockNumber: 9,
+              returnValues: {
+                validator: validator3
+              },
+              event: 'ValidatorAdded'
+            }
+          ]
+        } else {
+          return []
+        }
+      }),
+      methods: methodsBuilder(currentValidators)
+    } as unknown) as Contract
+
+    const snapshotProvider = ({
+      validatorAddedEvents: () => {
+        return []
+      },
+      validatorRemovedEvents: () => {
+        return []
+      },
+      snapshotBlockNumber: () => {
+        return 10
+      }
+    } as unknown) as SnapshotProvider
+
+    const list = await getValidatorList(contract, 15, snapshotProvider)
+
+    expect(list.length).toEqual(2)
+    expect(list).toEqual(expect.arrayContaining([validator1, validator2]))
+    expect(contract.getPastEvents).toBeCalledTimes(2)
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorAdded', {
+      fromBlock: 15
+    })
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorRemoved', {
+      fromBlock: 15
+    })
+  })
+  test('If validator was removed later from snapshot it should include it', async () => {
+    const currentValidators = [validator1, validator2]
+    const contract = ({
+      getPastEvents: jest.fn().mockImplementation(() => []),
+      methods: methodsBuilder(currentValidators)
+    } as unknown) as Contract
+
+    const snapshotProvider = ({
+      validatorAddedEvents: () => {
+        return []
+      },
+      validatorRemovedEvents: () => {
+        return [
+          {
+            blockNumber: 9,
+            returnValues: {
+              validator: validator3
+            },
+            event: 'ValidatorRemoved'
+          }
+        ]
+      },
+      snapshotBlockNumber: () => {
+        return 10
+      }
+    } as unknown) as SnapshotProvider
+
+    const list = await getValidatorList(contract, 5, snapshotProvider)
+
+    expect(list.length).toEqual(3)
+    expect(list).toEqual(expect.arrayContaining([validator1, validator2, validator3]))
+    expect(contract.getPastEvents).toBeCalledTimes(2)
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorAdded', {
+      fromBlock: 11
+    })
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorRemoved', {
+      fromBlock: 11
+    })
+  })
+  test('If validator was removed later from chain it should include it', async () => {
+    const currentValidators = [validator1, validator2]
+    const contract = ({
+      getPastEvents: jest.fn().mockImplementation(event => {
+        if (event === 'ValidatorRemoved') {
+          return [
+            {
+              blockNumber: 9,
+              returnValues: {
+                validator: validator3
+              },
+              event: 'ValidatorRemoved'
+            }
+          ]
+        } else {
+          return []
+        }
+      }),
+      methods: methodsBuilder(currentValidators)
+    } as unknown) as Contract
+
+    const snapshotProvider = ({
+      validatorAddedEvents: () => {
+        return []
+      },
+      validatorRemovedEvents: () => {
+        return []
+      },
+      snapshotBlockNumber: () => {
+        return 10
+      }
+    } as unknown) as SnapshotProvider
+
+    const list = await getValidatorList(contract, 15, snapshotProvider)
+
+    expect(list.length).toEqual(3)
+    expect(list).toEqual(expect.arrayContaining([validator1, validator2, validator3]))
+    expect(contract.getPastEvents).toBeCalledTimes(2)
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorAdded', {
+      fromBlock: 15
+    })
+    expect(contract.getPastEvents).toHaveBeenCalledWith('ValidatorRemoved', {
+      fromBlock: 15
+    })
   })
 })
