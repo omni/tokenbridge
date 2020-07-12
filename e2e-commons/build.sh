0137f5e36f6dc18404474338fc5fb08657d7f567
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-07-12 18:15:35 +0700

    Optimize e2e testing in ci

--- e2e-commons/build.sh
+++ e2e-commons/build.sh
@@ -6,10 +6,12 @@ docker-compose build parity1 parity2 blocks e2e
 
 while [ "$1" != "" ]; do
   if [ "$1" == "oracle" ]; then
-    docker-compose build oracle oracle-erc20 oracle-erc20-native oracle-amb
+    docker-compose pull oracle
   elif [ "$1" == "monitor" ]; then
-    docker-compose build monitor monitor-erc20 monitor-erc20-native monitor-amb
+    docker-compose pull monitor
   elif [ "$1" == "ui" ]; then
+    docker-compose pull ui
+    # this should only rebuild last 3 steps from ui/Dockerfile
     docker-compose build ui ui-erc20 ui-erc20-native ui-amb-stake-erc20-erc20
   fi
   shift
