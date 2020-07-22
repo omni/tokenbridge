8d8762eade71fd4ded07826824439183c7b30797
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-07-22 16:45:21 -0300

    Fix jest version

--- alm-e2e/src/test.js
+++ alm-e2e/src/test.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer')
 const { waitUntil } = require('./utils/utils')
 
+jest.setTimeout(60000)
+
 const statusText = 'Success'
 const statusSelector = 'label[data-id="status"]'
+
+const homeToForeignTxURL = 'http://localhost:3004/77/0x58e7d63368335b9591d4dbb43889084f698fcee93ab7656fd7a39d8c66bc4b60'
+const foreignToHomeTxURL = 'http://localhost:3004/42/0x592bf28fc896419d2838f71cd0388775814b692688f1ecd5b1519081566b994a'
 
 describe('ALM', () => {
   let browser
@@ -18,12 +23,12 @@ describe('ALM', () => {
   })
 
   it('should be titled "AMB Live Monitoring"', async () => {
-    await page.goto('http://localhost:3004/42/0x592bf28fc896419d2838f71cd0388775814b692688f1ecd5b1519081566b994a')
+    await page.goto(foreignToHomeTxURL)
 
     await expect(page.title()).resolves.toMatch('AMB Live Monitoring')
   })
   it('should display information of foreign to home transaction', async () => {
-    await page.goto('http://localhost:3004/42/0x592bf28fc896419d2838f71cd0388775814b692688f1ecd5b1519081566b994a')
+    await page.goto(foreignToHomeTxURL)
 
     await page.waitForSelector(statusSelector)
     await waitUntil(async () => {
@@ -33,7 +38,7 @@ describe('ALM', () => {
     })
   })
   it('should display information of home to foreign transaction', async () => {
-    await page.goto('http://localhost:3004/77/0x58e7d63368335b9591d4dbb43889084f698fcee93ab7656fd7a39d8c66bc4b60')
+    await page.goto(homeToForeignTxURL)
 
     await page.waitForSelector(statusSelector)
     await waitUntil(async () => {
