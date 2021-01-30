5a15b9c0ce5a5b7253edb93f129dc4b8bec3a1d0
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-01-30 20:15:03 +0300

    Display pretty error messages on manual execution reverts

--- alm/src/config/constants.ts
+++ alm/src/config/constants.ts
@@ -66,3 +66,12 @@ export const VALIDATOR_CONFIRMATION_STATUS = {
 export const SEARCHING_TX = 'Searching Transaction...'
 
 export const INCORRECT_CHAIN_ERROR = `Incorrect chain chosen. Switch to ${FOREIGN_NETWORK_NAME} in the wallet.`
+
+export const DOUBLE_EXECUTION_ATTEMPT_ERROR = `Your execution transaction has been reverted.
+However, the execution completed successfully in the transaction sent by a different party.`
+
+export const EXECUTION_FAILED_ERROR = `Your execution transaction has been reverted.
+Please, contact the support by messaging on %linkhttps://forum.poa.network/c/support`
+
+export const EXECUTION_OUT_OF_GAS_ERROR = `Your execution transaction has been reverted due to Out-of-Gas error.
+Please, resend the transaction and provide more gas for the transaction.`
