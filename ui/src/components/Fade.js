8fe4e7c0f4eacbdd14496d8419145afba012004a
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-04-14 18:39:17 +0300

    Remove ui dirs

--- ui/src/components/Fade.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { CSSTransition } from 'react-transition-group'
-
-export const Fade = ({ children, ...props }) => (
-  <CSSTransition {...props} timeout={1000} classNames="fade">
-    {children}
-  </CSSTransition>
-)
--- ui/src/components/utils/localstorage.js
+++
@@ -1,9 +0,0 @@
-export const DISCLAIMER_KEY = 'bridge-ui-app-disclaimer'
-
-export const setItem = (key, data) => {
-  localStorage.setItem(key, data)
-}
-
-export const getItem = key => {
-  return localStorage.getItem(key)
-}
