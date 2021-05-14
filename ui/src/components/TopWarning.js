a9ed614974b053e0b5fc1565d47ab9ad3b194f35
Ankit Bajaj <abajaj@deqode.com>
2021-05-14 16:35:33 +0530

    DV-674 Add close icon to top warning message

--- ui/src/components/TopWarning.js
+++ ui/src/components/TopWarning.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TopWarning = () => {
+  const [isClose, setIsClose] = useState(localStorage.getItem("top-warning-close"));
+  const handleClose = () => {
+    localStorage.setItem("top-warning-close", true);
+    setIsClose(true)
+  }
   return (
-    <div className="top-warning">
-      <img className="top-warning-img" src={require('../assets/images/icons/vector.png')} />
-      <span className="top-warning-text">Always make sure the URL is bridge.pumapay.io - Press (Ctrl+D or Cmd+D) to bookmark it to be safe.</span>
-    </div>
+    <>
+    {!isClose ? <div className="top-warning">
+      <div className="top-warning-detail">
+        <img className="top-warning-img" src={require('../assets/images/icons/vector.png')} />
+        <span className="top-warning-text">Always make sure the URL is bridge.pumapay.io - Press (Ctrl+D or Cmd+D) to bookmark it to be safe.</span>
+      </div>
+      <span className="top-warning-close" onClick={handleClose}>X</span>
+    </div>: null}
+    </>
   )
 }
