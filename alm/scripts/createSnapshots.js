d5e884cf0e6cf5f743ff4c1e1d936cd0b45bbfc3
t0m <husamirami@gmail.com>
2021-06-13 22:39:41 +0300

    fixed small mistake in snapshots, fixed api urls

--- alm/scripts/createSnapshots.js
+++ alm/scripts/createSnapshots.js
@@ -104,18 +104,17 @@ const generateSnapshot = async (side, url, bridgeAddress) => {
   fs.writeFileSync(snapshotFullPath, JSON.stringify(snapshot, null, 2))
 }
 
-const isNonEmptyString = (str) => {
-  if(typeof str !== 'string' || str === '') 
-    return true;
-  return false;
+const isNonEmptyString = str => {
+  if (typeof str !== 'string' || str === '') return false
+  return true
 }
 
 const main = async () => {
-  let promises = [];
-  // If we're using subgraphs, there is no need for snapshots. This solution was introduced because of the binance rpc limits for eth_getLogs method
-  if(!isNonEmptyString(HOME_SUBGRAPH_URL))
+  let promises = []
+  // If we're using subgraphs, there is no need for snapshots. This solution was introduced because of the bsc rpc limits for eth_getLogs method
+  if (!isNonEmptyString(HOME_SUBGRAPH_URL))
     promises.push(generateSnapshot('home', COMMON_HOME_RPC_URL, COMMON_HOME_BRIDGE_ADDRESS))
-  if(!isNonEmptyString(FOREIGN_SUBGRAPH_URL))
+  if (!isNonEmptyString(FOREIGN_SUBGRAPH_URL))
     promises.push(generateSnapshot('foreign', COMMON_FOREIGN_RPC_URL, COMMON_FOREIGN_BRIDGE_ADDRESS))
   await Promise.all(promises)
 }
